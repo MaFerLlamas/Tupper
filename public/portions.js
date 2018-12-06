ab6c4c05fab9688ec2abaa782740071af78ad9a6
MaFerLlamas <fernandaavilallamas@gmail.com>
2018-12-05 21:49:51 -0600

    counters ready

--- public/portions.js
+++ public/portions.js
@@ -1,3 +1,59 @@
+
+function classToName(nameclass){
+    let aux;
+    switch(nameclass){
+      case "vegetables":
+        aux="vegetales";
+        break;
+      case "fruits":
+        aux="frutas";
+        break;
+      case "cereal1":
+        aux="cereal S/G";
+        break;
+      case "cereal2":
+        aux="cereal C/G";
+        break;
+      case "legume":
+        aux="legumbre";
+        break;
+      case "animalA":
+        aux="AOA A";
+        break;
+      case "animalB":
+        aux="AOA B";
+        break;
+      case "animalC":
+        aux="AOA C";
+        break;
+      case "animalD":
+        aux="AOA D";
+        break;
+      case "milkA":
+        aux="Leche A";
+        break;
+      case "milkB":
+        aux="Leche B";
+        break;
+      case "milkC":
+        aux="Leche C";
+        break;
+      case "milkD":
+        aux="Leche D";
+        break;
+      case "gras1":
+        aux="grasa C/P";
+        break;
+      case "gras2":
+        aux="grasa S/P";
+        break;
+      case "sugar1":
+        aux="Azúcar S/G";
+        break;
+    }
+    return aux;
+  }
+
 let portions = 
 {
     frutas: {
--- public/tupperScript.js
+++ public/tupperScript.js
@@ -2,48 +2,109 @@ let tupperDB = firebase.database();
 // switch
 // if is on follow the rules
 let btnSwitch = document.querySelector('.switch');
-let isStrict = false;
+let isStrict = true;
 let numGroups = 16;
 let containerCounter = document.querySelectorAll('.container-counter');
-let foodFrame = {
+
+let counters = {
   
-  vegetable: 0,
+  vegetables: 0,
   fruits: 0,
   cereal1: 0,
   cereal2: 0,
   legume: 0,
-  animalA: 1,
+  animalA: 0,
   animalB: 0,
   animalC: 0,
   animalD: 0,
   milkA: 0,
   milkB: 0,
-  milkC: 1,
+  milkC: 0,
   milkD: 0,
   gras1: 0,
   gras2: 0,
   sugar1: 0
 
 }
-showPortions();
+
+let userPlan = {
+  morning: {
+    cereal1: 2,
+    animalA: 2,
+    gras2: 2
+  },
+  snack1: {
+    fruits: 2,
+    gras1: 1
+  },
+  afternoon: {
+    vegetables: 1,
+    cereal1: 2,
+    animalB: 1,
+    gras2: 2
+  },
+  snack2: {
+    fruits: 1,
+    milkA: 1,
+    gras1: 1
+  },
+  evening:{
+    vegetables: 2,
+    cereal1: 1,
+    animalB: 1,
+    gras2: 1
+  }
+
+}
+
+initialize();
+
+
+function takeUserPlan(){
+  // iterates 5 types of meal
+  let actualMeal;
+  let aux;
+  for(i in userPlan){
+    actualMeal = document.querySelector('#' + i);
+    // actualMeal.innerHTML = "";
+    
+    // iterates through the group content in every meal
+    for (e in userPlan[i]){
+      counters[e] += userPlan[i][e];
+      console.log(e);
+      aux += `<div class="portion ${e}">
+				CEREAL
+      </div>`
+      console.log("meal" + actualMeal);
+      console.log("group" + aux);
+      actualMeal.innerHTML += aux;
+      aux = ``;
+    }
+  }
+}
+
 function showPortions(){
   let cont = 0;
-  for(i in foodFrame){
-    if (foodFrame[i] > 0){
-      containerCounter[cont].querySelector('.lbl-counter').innerHTML = foodFrame[i];
+  for(i in counters){
+    containerCounter[cont].classList.remove('visible');
+    if (isStrict){
+      if (counters[i] > 0){
+        containerCounter[cont].querySelector('.counter').innerHTML = counters[i];
+        containerCounter[cont].classList.add('visible');
+      }
+    }else{
+      containerCounter[cont].querySelector('.counter').innerHTML = counters[i];
       containerCounter[cont].classList.add('visible');
     }
     cont++;
   }
-
-  
- 
 }
 
 btnSwitch.addEventListener('click',function(){
   isStrict = !isStrict;
   btnSwitch.classList.toggle('on');
   btnSwitch.classList.toggle('off');
+  showPortions();
 });
 
 function writePortionData() {
@@ -57,7 +118,8 @@ function writePortionData() {
 
  function initialize(){
   isEstrict = false;
-
+  takeUserPlan();
+  showPortions();
  }
 
 
